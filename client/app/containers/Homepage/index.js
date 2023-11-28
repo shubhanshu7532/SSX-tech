2670d101f1145881c2f7dc5e724a9b4feb203599
shubhanshu7532 <69970271+shubhanshu7532@users.noreply.github.com>
2023-11-28 21:02:42 +0530

    some changes

---
+++ client/app/containers/Homepage/index.js
@@ -0,0 +1,60 @@
+/**
+ *
+ * Homepage
+ *
+ */
+
+import React from 'react';
+
+import { connect } from 'react-redux';
+import { Row, Col } from 'reactstrap';
+
+import actions from '../../actions';
+import banners from './banners.json';
+import CarouselSlider from '../../components/Common/CarouselSlider';
+import { responsiveOneItemCarousel } from '../../components/Common/CarouselSlider/utils';
+
+class Homepage extends React.PureComponent {
+  render() {
+    return (
+      <div className='homepage'>
+        <Row className='flex-row'>
+          <Col xs='12' lg='6' className='order-lg-2 mb-3 px-3 px-md-2'>
+            <div className='home-carousel'>
+              <CarouselSlider
+                swipeable={true}
+                showDots={true}
+                infinite={true}
+                autoPlay={false}
+                slides={banners}
+                responsive={responsiveOneItemCarousel}
+              >
+                {banners.map((item, index) => (
+                  <img key={index} src={item.imageUrl} />
+                ))}
+              </CarouselSlider>
+            </div>
+          </Col>
+          <Col xs='12' lg='3' className='order-lg-1 mb-3 px-3 px-md-2'>
+            <div className='d-flex flex-column h-100 justify-content-between'>
+              <img src='https://i1.wp.com/www.igniteengineers.com/wp-content/uploads/2018/05/Amazon.jpg?resize=768%2C512&ssl=1' className='mb-3' />
+              <img src='https://i1.wp.com/www.igniteengineers.com/wp-content/uploads/2018/05/Amazon.jpg?resize=768%2C512&ssl=1' />
+            </div>
+          </Col>
+          <Col xs='12' lg='3' className='order-lg-3 mb-3 px-3 px-md-2'>
+            <div className='d-flex flex-column h-100 justify-content-between'>
+              <img src='https://i1.wp.com/www.igniteengineers.com/wp-content/uploads/2018/05/Amazon.jpg?resize=768%2C512&ssl=1' className='mb-3' />
+              <img src='https://i1.wp.com/www.igniteengineers.com/wp-content/uploads/2018/05/Amazon.jpg?resize=768%2C512&ssl=1' />
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(mapStateToProps, actions)(Homepage);
